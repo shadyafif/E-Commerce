066770b5c2890eebb47491ec940b23fc1f6ad5fd
Eng SHADY <shadyafifghatts@gmail.com>
2021-10-19 20:46:11 +0200

    update Fragments Generate BaseFragment

---
+++ app/src/main/java/com/example/shopping/utilies/baseClasses/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.example.shopping.utilies.baseClasses
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+open class BaseFragment  <T : ViewBinding>(private val inflateMethod : (LayoutInflater, ViewGroup?, Boolean) -> T) : Fragment(){
+
+    private var _binding : T? = null
+
+    // This can be accessed by the child fragments
+    // Only valid between onCreateView and onDestroyView
+    val binding : T get() = _binding!!
+    open fun T.initialize(){}
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = inflateMethod.invoke(inflater, container, false)
+        binding.initialize()
+        // replaced _binding!! with binding
+        return binding.root
+    }
+
+    // Removing the binding reference when not needed is recommended as it avoids memory leak
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+}
